b12b1d68641c75117d94f08034a1c18da32b6742
Alistair Vu <alistairvu@gmail.com>
2021-03-25 18:31:45 +0700

    Add package.json

--- frontend/src/components/app/AppHeader.tsx
+++ frontend/src/components/app/AppHeader.tsx
@@ -1,10 +1,13 @@
 import Navbar from "react-bootstrap/Navbar"
+import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 
 const AppHeader: React.FC = () => {
   return (
     <Navbar bg="dark" expand="lg" variant="dark">
-      <Navbar.Brand>coffeeview</Navbar.Brand>
+      <Container>
+        <Navbar.Brand>coffeeview</Navbar.Brand>
+      </Container>
     </Navbar>
   )
 }
